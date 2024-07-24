ac57a9252fe4eb398a657bc1c63e8310156a6f88
DJUIC3 <artem.anokhin@gmail.com>
2024-07-24 17:06:22 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidth = 1;
+
+console.log(number);
+
+
+const man = {
+    name: "John",
+    age: 24,
+    isMarried: true
+};
+console.log(man.age);
+
+
+let arr = ['misha', 'matvii', 'alisa'];
+console.log(arr[2]);
---
+++ js/user_actions.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18 age?", 18);
+// console.log(typeof(answer));
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Как ваша фамилия?','');
+// answers[2] = prompt('Сколько Вам лет?','');
+
+// document.write(answers);
+
+
+
+const user = "Divan";
+alert(`Привет, ${user}`);
